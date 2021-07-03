7ca400ce5ef237b607e3c6f556e842130d2ccb6a
brian ryall <brian@brianryall.xyz>
2021-07-03 00:54:45 +0000

    add support for suckless software

--- systems/nixosvm/configuration.nix
+++ systems/nixosvm/configuration.nix
@@ -53,9 +53,43 @@
 
 
   # Enable the Plasma 5 Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  #services.xserver.displayManager.sddm.enable = true;
+  #services.xserver.desktopManager.plasma5.enable = true;
   
+  # Enable the DWM window manager
+  nixpkgs.overlays = [
+    (self: super: {
+      dwm = super.dwm.overrideAttrs(_: {
+        src = builtins.fetchGit {
+          url = "https://github.com/polarmutex/dwm";
+          rev = "f9e8c451445dc9dcf3408397c0a9d3324c2941f6";
+          ref = "custom";
+        };
+      });
+      st = super.st.overrideAttrs( oldAttrs : rec {
+        src = builtins.fetchGit {
+          url = "https://github.com/polarmutex/st";
+          rev = "8684c90a1d5a15491a52d763c5625c3479887df7";
+          ref = "custom";
+        };
+        buildInputs = oldAttrs.buildInputs ++ [ pkgs.harfbuzz.dev ];
+      });
+      dmenu = super.dmenu.overrideAttrs( oldAttrs : rec {
+        src = builtins.fetchGit {
+          url = "https://github.com/polarmutex/dmenu";
+          rev = "1e6783bc0bba32d402f6185e23702055b7bf0acf";
+          ref = "custom";
+        };
+      });
+    })
+  ];
+  services.xserver.windowManager.dwm.enable = true;
+  services.xserver = {
+    displayManager = {
+      defaultSession = "none+dwm";
+      lightdm.enable = true;
+    };
+  };
 
   # Configure keymap in X11
   services.xserver.layout = "us";
@@ -87,6 +121,8 @@
     zsh
     brave
     lazygit
+    dmenu
+    st
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
