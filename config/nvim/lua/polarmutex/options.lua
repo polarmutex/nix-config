145a6c54b8307dc4c37e3a229e71563a013f7dfe
Brian Ryall <brian@brianryall.xyz>
2021-07-14 15:37:34 -0400

    chore(neovim) add link to source

--- config/nvim/lua/polarmutex/options.lua
+++ config/nvim/lua/polarmutex/options.lua
@@ -49,15 +49,16 @@ opt.wrap = false -- Disable line wrap
 opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize" }
 
 -- TODO: w, {v, b, l}
-opt.formatoptions = opt.formatoptions - "a" -- Auto formatting is BAD.
-- "t" -- Don't auto format my code. I got linters for that.
-+ "c" -- In general, I like it when comments respect textwidth
-+ "q" -- Allow formatting comments w/ gq
-- "o" -- O and o, don't continue comments
-+ "r" -- But do continue when pressing enter.
-+ "n" -- Indent past the formatlistpat, not underneath it.
-+ "j" -- Auto-remove comments if possible.
-- "2" -- I'm not in gradeschool anymore
+opt.formatoptions = opt.formatoptions
+    - "a" -- Auto formatting is BAD.
+    - "t" -- Don't auto format my code. I got linters for that.
+    + "c" -- In general, I like it when comments respect textwidth
+    + "q" -- Allow formatting comments w/ gq
+    - "o" -- O and o, don't continue comments
+    + "r" -- But do continue when pressing enter.
+    + "n" -- Indent past the formatlistpat, not underneath it.
+    + "j" -- Auto-remove comments if possible.
+    - "2" -- I'm not in gradeschool anymore
 
 opt.shortmess = opt.shortmess - "S" -- Show Search count
 
