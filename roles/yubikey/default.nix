88dab4072430ce9aebc8499b61f35bff627e2807
Brian Ryall <brian@brianryall.xyz>
2021-07-14 16:46:21 -0400

    feat: add gpg and yubikey support

--- roles/yubikey/default.nix
+++ roles/yubikey/default.nix
@@ -13,11 +13,14 @@
   environment.shellInit = ''
     export GPG_TTY="$(tty)"
     gpg-connect-agent /bye
+    export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
   '';
 
   programs = {
+    ssh.startAgent = false;
     gnupg.agent = {
       enable = true;
+      enableSSHSupport = true;
       pinentryFlavor = "gtk2";
     };
   };
