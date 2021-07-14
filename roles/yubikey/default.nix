d7133aad15b253cff2d4c624525099084e4ab190
Brian Ryall <brian@brianryall.xyz>
2021-07-13 23:58:22 -0400

    feat: add gpg and yubikeey support

---
+++ roles/yubikey/default.nix
@@ -0,0 +1,24 @@
+{config, pkgs, lib, ...}:
+{
+  environment.systemPackages = with pkgs; [
+    gnupg
+    yubikey-personalization
+    yubioath-desktop
+    pinentry-gtk2
+  ];
+
+  services.udev.packages = [ pkgs.yubikey-personalization ];
+  services.pcscd.enable = true;
+
+  environment.shellInit = ''
+    export GPG_TTY="$(tty)"
+    gpg-connect-agent /bye
+  '';
+
+  programs = {
+    gnupg.agent = {
+      enable = true;
+      pinentryFlavor = "gtk2";
+    };
+  };
+}
