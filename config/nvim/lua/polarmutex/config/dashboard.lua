9acd4fc3978f3c691fe4bc6d39f252e320abeb80
Brian Ryall <brian@brianryall.xyz>
2021-07-14 15:08:14 -0400

    feat(neovim): add dashboard

---
+++ config/nvim/lua/polarmutex/config/dashboard.lua
@@ -0,0 +1,30 @@
+vim.g.dashboard_default_executive = "telescope"
+-- vim.g.dashboard_preview_command = "splashcii"
+vim.g.dashboard_preview_pipeline = "lolcat"
+-- vim.g.dashboard_preview_file = "beach"
+-- vim.g.dashboard_preview_file_height = 20
+-- vim.g.dashboard_preview_file_width = 80
+vim.g.dashboard_enable_session = 0
+vim.g.dashboard_disable_statusline = 0
+
+vim.g.dashboard_custom_header = {
+    "     __    __",
+    "    /_/ /\\ \\_\\",
+    "   __ \\ \\/ / __",
+    "   \\_\\_\\/\\/_/_/       ____        __           __  ___      __",
+    "/\\___\\_\\/_/___/\\     / __ \\____  / /___ ______/  |/  /_  __/ /____  _  __",
+    "\\/ __/_/\\_\\__ \\/    / /_/ / __ \\/ / __ `/ ___/ /|_/ / / / / __/ _ \\| |/_/",
+    "  /_/ /\\/\\ \\_\\     / ____/ /_/ / / /_/ / /  / /  / / /_/ / /_/  __/>  <",
+    "   __/ /\\ \\__     /_/    \\____/_/\\__,_/_/  /_/  /_/\\__,_/\\__/\\___/_/|_|",
+    "   \\_\\ \\/ /_/",
+}
+
+vim.g.dashboard_custom_shortcut = {
+    ["last_session"] = "SPC s l",
+    ["find_history"] = "SPC f r",
+    ["find_file"] = "SPC spc",
+    ["new_file"] = "SPC f n",
+    ["change_colorscheme"] = "SPC h c",
+    ["find_word"] = "SPC f g",
+    ["book_marks"] = "SPC f b",
+}
