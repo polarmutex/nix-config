d11d5a7cb01ced3e5ce9f9cb08dc72a32fdba1df
Brian Ryall <brian@brianryall.xyz>
2021-09-18 14:35:01 -0400

    fix: update tree-sitter-svelte to latest

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,6 +5,9 @@ with pkgs;
   tree-sitter-beancount = (
     callPackage ./tree-sitter-beancount/default.nix {}
   );
+  tree-sitter-svelte = (
+    callPackage ./tree-sitter-svelte/default.nix {}
+  );
   fathom = (callPackage ./fathom/default.nix {});
   logseq = (callPackage ./logseq/default.nix {});
   obsidian = (callPackage ./obsidian/default.nix {});
