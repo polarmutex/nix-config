c239b9005e0c4bc0528b838636bdfe4e5c7a53b3
Brian Ryall <brian@brianryall.xyz>
2021-09-18 14:35:01 -0400

    fix: update tree-sitter-svelte to latest

--- overlays/default.nix
+++ overlays/default.nix
@@ -8,6 +8,7 @@ self: super: {
   my = {
     jdtls = super.callPackage ../pkgs/jdt-language-server {};
     tree-sitter-beancount = super.callPackage ../pkgs/tree-sitter-beancount {};
+    tree-sitter-svelte = super.callPackage ../pkgs/tree-sitter-svelte {};
     fathom = super.callPackage ../pkgs/fathom {};
     logseq = super.callPackage ../pkgs/logseq {};
     obsidian = super.callPackage ../pkgs/obsidian {};
