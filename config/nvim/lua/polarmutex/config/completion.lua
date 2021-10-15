3e6a0290a76879369fc989791d378e44224d1891
Brian Ryall <brian@brianryall.xyz>
2021-10-15 15:35:07 -0400

    update: cmp config

--- config/nvim/lua/polarmutex/config/completion.lua
+++ config/nvim/lua/polarmutex/config/completion.lua
@@ -11,13 +11,11 @@ cmp.setup({
         end,
     },
     mapping = {
-        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-d>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.close(),
-        ["<c-y>"] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Insert,
-            select = true,
-        }),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
     },
     sources = {
         --{ name = "buffer" },
