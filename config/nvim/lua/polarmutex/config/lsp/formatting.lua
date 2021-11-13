76df6775a100b3c12b925cbd1ae41cc9364311bc
Brian Ryall <brian@brianryall.xyz>
2021-11-13 13:16:18 -0500

    fix: null-ls formatting issue

--- config/nvim/lua/polarmutex/config/lsp/formatting.lua
+++ config/nvim/lua/polarmutex/config/lsp/formatting.lua
@@ -26,16 +26,8 @@ end
 function M.setup(client, buf)
     local ft = vim.api.nvim_buf_get_option(buf, "filetype")
     local nls = require("polarmutex.config.lsp.null-ls")
-    local efm_formatted = require("polarmutex.config.lsp.efm").formatted_languages
 
-    local enable = false
-    if nls.has_formatter(ft) then
-        enable = client.name == "null-ls"
-    elseif efm_formatted[ft] then
-        enable = client.name == "efm"
-    else
-        enable = not (client.name == "efm" or client.name == "null-ls")
-    end
+    local enable = true
 
     client.resolved_capabilities.document_formatting = enable
     -- format on save
