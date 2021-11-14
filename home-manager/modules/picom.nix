c89ab55f2607ce99f78c1c4213c61ed0247fdbc7
Brian Ryall <brian@brianryall.xyz>
2021-11-14 15:29:05 -0500

    fix: picom issues

--- home-manager/modules/picom.nix
+++ home-manager/modules/picom.nix
@@ -1,13 +1,36 @@
 { pkgs, config, lib, ... }:
 {
-  home.packages = with pkgs; [
-    picom
-  ];
-
   services.picom = {
+    package = pkgs.picom-next;
     enable = true;
     shadow = true;
     backend = "glx";
-    activeOpacity = "1.0";
+    activeOpacity = "0.9";
+    experimentalBackends = true;
+    extraOptions = ''
+      detect-client-opacity = true;
+      detect-rounded-corners = true;
+      blur:
+      {
+          method = "kawase";
+          strength = 8;
+          background = true;
+          background-frame = false;
+          background-fixed = false;
+      };
+      blur-background-exclude = [
+          "class_g = 'keynav'"
+      ];
+      corner-radius = 18;
+      rounded-corners-exclude = [
+          "window_type = 'dock'",
+          "_NET_WM_STATE@:32a *= '_NET_WM_STATE_FULLSCREEN'",
+          "class_g = 'keynav'",
+      ];
+      round-borders = 1;
+      round-borders-exclude = [
+          "class_g = 'keynav'"
+      ];
+    '';
   };
 }
