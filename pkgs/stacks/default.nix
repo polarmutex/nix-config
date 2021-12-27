65aa775e55774f0ac994fb1b0e54137cd87c8f5d
Brian Ryall <brian@brianryall.xyz>
2021-12-26 22:02:14 -0500

    fix: gsettings error

--- pkgs/stacks/default.nix
+++ pkgs/stacks/default.nix
@@ -1,4 +1,4 @@
-{ lib, fetchurl, appimageTools }:
+{ pkgs, lib, fetchurl, appimageTools }:
 appimageTools.wrapType2 rec {
   pname = "stacks-task-manager";
   version = "1.6.3";
@@ -7,6 +7,10 @@ appimageTools.wrapType2 rec {
     url = "https://github.com/stacks-task-manager/stacks/releases/download/v${version}/Stacks-Linux-${version}.AppImage";
     sha256 = "sha256-WC2qpr2B3hTyyJwQziSArrB0M41wduxPPcUHbPlmmcI=";
   };
+
+  profile = ''
+    export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
+  '';
 
   meta = with lib; {
     description = "Your personal kanban to do and project manager";
