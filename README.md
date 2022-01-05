6d84989b384c340cc08396ed5ee9e9713711dbe1
Brian Ryall <brian@brianryall.xyz>
2022-01-04 23:04:04 -0500

    chore: updates

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 Took inspiration from the following:
 
-* [willtaylor](https://github.com/willtaylor/dotfiles)
-* [pinpox](https://github.com/pinpox/nixos)
+- [willtaylor](https://github.com/willtaylor/dotfiles)
+- [pinpox](https://github.com/pinpox/nixos)
+- [Gerschtli](https://github.com/Gerschtli/nix-config)
+- [Misterio77](https://github.com/Misterio77/nix-config)
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,16 +1,12 @@
 { pkgs, ... }:
 with pkgs;
 {
-  jdtls = (callPackage ./jdt-language-server/default.nix { });
-  tree-sitter-beancount = (
-    callPackage ./tree-sitter-beancount/default.nix { }
-  );
-  tree-sitter-svelte = (
-    callPackage ./tree-sitter-svelte/default.nix { }
-  );
-  fathom = (callPackage ./fathom/default.nix { });
-  logseq = (callPackage ./logseq/default.nix { });
-  obsidian = (callPackage ./obsidian/default.nix { });
-  dwm = (callPackage ./dwm/default.nix { });
-  prettierd = (callPackage ./prettierd/default.nix { });
+  jdtls = callPackage ./jdt-language-server/default.nix { };
+  tree-sitter-beancount = callPackage ./tree-sitter-beancount/default.nix { };
+  tree-sitter-svelte = callPackage ./tree-sitter-svelte/default.nix { };
+  fathom = callPackage ./fathom/default.nix { };
+  logseq = callPackage ./logseq/default.nix { };
+  obsidian = callPackage ./obsidian/default.nix { };
+  dwm = callPackage ./dwm/default.nix { };
+  prettierd = callPackage ./prettierd/default.nix { };
 }
