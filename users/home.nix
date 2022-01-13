01891ed9dff6ddb0457a360d0388556af4325a73
Brian Ryall <brian@brianryall.xyz>
2022-01-13 16:04:41 -0500

    refactor: to users dir

---
+++ users/home.nix
@@ -0,0 +1,25 @@
+_:
+{
+  programs.home-manager.enable = true;
+  home.sessionVariables = { EDITOR = "nvim"; };
+  home.stateVersion = "20.09";
+
+  imports = [
+    ./modules/temp.nix
+    ./modules/work.nix
+    ./modules/fonts.nix
+    ./modules/programs/git.nix
+    ./modules/programs/zsh.nix
+    ./modules/programs/tmux.nix
+    ./modules/programs/neovim.nix
+    ./modules/programs/firefox/firefox.nix
+    ./modules/services/picom.nix
+    ./modules/services/logseq.nix
+    ./modules/services/spotify.nix
+    ./modules/services/obsidian.nix
+    ./modules/services/protonvpn.nix
+    ./modules/services/dwm-status.nix
+    ./modules/services/wallpapers.nix
+    ./modules/services/gpg/default.nix
+  ];
+}
