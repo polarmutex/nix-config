b5ec763ddcfb110f5c6d529cb2346f7a349edeee
Brian Ryall <brian@brianryall.xyz>
2022-01-14 16:47:50 -0500

    fix: polarbear config to get running

--- hosts/polarbear/configuration.nix
+++ hosts/polarbear/configuration.nix
@@ -4,14 +4,11 @@
 
   polar = {
 
-    base.general.hostname = "blackbear";
+    base.general.hostname = "polarbear";
 
-    #base.desktop = {
-    #  enable = false;
-    #  laptop = false;
-    #};
-    base.server = {
-      enable = false;
+    base.desktop = {
+      enable = true;
+      laptop = false;
     };
 
     system = {
@@ -21,14 +18,14 @@
 
   time.hardwareClockInLocalTime = true;
 
-  #services.xserver.videoDrivers = [ "nvidia" ];
+  services.xserver.videoDrivers = [ "nvidia" ];
   #services.printing.enable = true;
-  #services.tailscale.enable = true;
+  services.tailscale.enable = true;
   #hardware.sane.enable = true;
 
   #virtualisation.docker.enable = true;
   #programs.dconf.enable = true;
-  #environment.systemPackages = with pkgs; [ tailscale ];
+  environment.systemPackages = with pkgs; [ tailscale ];
   #virtualisation.virtualbox.host.enable = true;
   #virtualisation.virtualbox.host.enableExtensionPack = true;
   #users.extraGroups.vboxusers.members = [ "polar" ];
--- hosts/polarbear/hm.nix
+++ hosts/polarbear/hm.nix
@@ -1,51 +1,51 @@
-_:
+{ pkgs, ... }:
 {
-  #polar = {
-  #  programs = {
-  #    firefox.enable = true;
-  #    git.enable = true;
-  #    neovim.enable = true;
-  #    tmux.enable = true;
-  #    zsh.enable = true;
-  #  };
-  #  services = {
-  #    picom.enable = true;
-  #    wallpapers.enable = true;
-  #  };
-  #};
+  polar = {
+    programs = {
+      firefox.enable = true;
+      git.enable = true;
+      neovim.enable = true;
+      tmux.enable = true;
+      zsh.enable = true;
+    };
+    services = {
+      picom.enable = true;
+      wallpapers.enable = true;
+    };
+  };
 
-  #home.packages = with pkgs; [
-  #_1password
-  #  _1password-gui
-  #  anki-bin
-  #  arandr
-  #  appimage-run
-  #  brave
-  #  exa
-  #  gcc
-  #  htop
-  #  fd
-  #  networkmanagerapplet
-  #  nix-index
-  #  pavucontrol
-  #  playerctl
-  #  spotify
-  #  xfce.exo # thunar "open terminal here"
-  #  xfce.thunar
-  #  xfce.thunar-archive-plugin
-  #  xfce.thunar-volman
-  #  xfce.tumbler # thunar thumbnails
-  #  xfce.xfce4-volumed-pulse
-  #  xfce.xfconf # thunar save settings
-  #  xorg.xrandr
-  #  yubioath-desktop
-  #  ytfzf
-  #  element-desktop
-  #  qmk
-  #  zoom-us
-  #  ansible
-  #  peek
-  #  ];
+  home.packages = with pkgs; [
+    _1password
+    _1password-gui
+    anki-bin
+    arandr
+    appimage-run
+    brave
+    exa
+    gcc
+    htop
+    fd
+    networkmanagerapplet
+    nix-index
+    pavucontrol
+    playerctl
+    spotify
+    xfce.exo # thunar "open terminal here"
+    xfce.thunar
+    xfce.thunar-archive-plugin
+    xfce.thunar-volman
+    xfce.tumbler # thunar thumbnails
+    xfce.xfce4-volumed-pulse
+    xfce.xfconf # thunar save settings
+    xorg.xrandr
+    #yubioath-desktop
+    ytfzf
+    element-desktop
+    qmk
+    zoom-us
+    ansible
+    peek
+  ];
 
   # Environment variables
   # systemd.user.sessionVariables = { ZDOTDIR = "/home/polar/.config/zsh"; };
