916c38fed595b3f3ca685c40d4f2023f7ebece6e
Brian Ryall <brian@brianryall.xyz>
2022-01-28 16:11:51 -0500

    feat: add kanagawa theme

--- config/nvim/lua/polarmutex/config/theme.lua
+++ config/nvim/lua/polarmutex/config/theme.lua
@@ -1,3 +1,6 @@
-local config = require("polarmutex.colorschemes.tokyonight.config")
-config.style = "night"
-require("polarmutex.colorschemes.tokyonight").colorscheme(config)
+--local config = require("polarmutex.colorschemes.tokyonight.config")
+--config.style = "night"
+--require("polarmutex.colorschemes.tokyonight").colorscheme(config)
+
+local config = require("polarmutex.colorschemes.kanagawa.config")
+require("polarmutex.colorschemes.kanagawa").colorscheme(config)
