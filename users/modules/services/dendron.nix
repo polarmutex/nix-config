b17512fc15d4e66162ba65d53a730f7e2b49dd42
Brian Ryall <brian@brianryall.xyz>
2022-02-01 16:51:06 -0500

    feat: add dendron service

---
+++ users/modules/services/dendron.nix
@@ -0,0 +1,58 @@
+{ config, pkgs, lib, ... }:
+with lib;
+let
+  cfg = config.polar.services.dendron;
+in
+{
+  ###### interface
+
+  options = {
+    polar.services.dendron = {
+      enable = mkOption {
+        type = types.bool;
+        default = false;
+        description = "Enable dendron";
+      };
+    };
+  };
+
+  ###### implementation
+
+  config = mkIf cfg.enable {
+
+    home.packages = with pkgs; [
+    ];
+
+    systemd.user.services.devlog-sync = {
+      Unit = { Description = "devlog sync"; };
+      Service = {
+        CPUSchedulingPolicy = "idle";
+        IOSchedulingClass = "idle";
+        ExecStart = toString (
+          pkgs.writeShellScript "devlog-sync" ''
+            #!/usr/bin/env sh
+            DEVLOG_PATH="$HOME/repos/personal/devlog"
+            cd $DEVLOG_PATH
+            CHANGES_EXIST="$(${pkgs.git}/bin/git status - porcelain | ${pkgs.coreutils}/bin/wc -l)"
+            if [ "$CHANGES_EXIST" -eq 0 ]; then
+              exit 0
+            fi
+            ${pkgs.git}/bin/git pull
+            ${pkgs.git}/bin/git add .
+            ${pkgs.git}/bin/git commit -q -m "Last Sync: $(${pkgs.coreutils}/bin/date +"%Y-%m-%d %H:%M:%S")"
+            ${pkgs.git}/bin/git push -q
+          ''
+        );
+      };
+    };
+
+    systemd.user.timers.devlog-sync = {
+      Unit = { Description = "devlog periodic sync"; };
+      Timer = {
+        Unit = "devlog-sync.service";
+        OnCalendar = "*:0/30";
+      };
+      Install = { WantedBy = [ "timers.target" ]; };
+    };
+  };
+}
