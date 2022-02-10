e6dfb0b495ffce615d46614a86f30c5cc6edd2b0
Brian Ryall <brian@brianryall.xyz>
2022-02-10 13:45:26 -0500

    fix: add work option to dendron

--- users/modules/services/dendron.nix
+++ users/modules/services/dendron.nix
@@ -13,78 +13,89 @@ in
         default = false;
         description = "Enable dendron";
       };
+      work = mkOption {
+        type = types.bool;
+        default = false;
+        description = "work mode";
+      };
     };
   };
 
   ###### implementation
 
-  config = mkIf cfg.enable {
+  config = mkIf cfg.enable
+    (mkMerge [
+      {
 
-    home.packages = with pkgs; [
-    ];
+        home.packages = with pkgs; [
+        ];
 
-    systemd.user.services.devlog-sync = {
-      Unit = { Description = "devlog sync"; };
-      Service = {
-        CPUSchedulingPolicy = "idle";
-        IOSchedulingClass = "idle";
-        ExecStart = toString (
-          pkgs.writeShellScript "devlog-sync" ''
-            #!/usr/bin/env sh
-            DEVLOG_PATH="$HOME/repos/personal/devlog"
-            cd $DEVLOG_PATH
-            CHANGES_EXIST="$(${pkgs.git}/bin/git status - porcelain | ${pkgs.coreutils}/bin/wc -l)"
-            if [ "$CHANGES_EXIST" -eq 0 ]; then
-              exit 0
-            fi
-            ${pkgs.git}/bin/git pull
-            ${pkgs.git}/bin/git add .
-            ${pkgs.git}/bin/git commit -q -m "Last Sync: $(${pkgs.coreutils}/bin/date +"%Y-%m-%d %H:%M:%S")"
-            ${pkgs.git}/bin/git push -q
-          ''
-        );
-      };
-    };
+        systemd.user.services.devlog-sync = {
+          Unit = { Description = "devlog sync"; };
+          Service = {
+            CPUSchedulingPolicy = "idle";
+            IOSchedulingClass = "idle";
+            ExecStart = toString (
+              pkgs.writeShellScript "devlog-sync" ''
+                #!/usr/bin/env sh
+                DEVLOG_PATH="$HOME/repos/personal/devlog"
+                cd $DEVLOG_PATH
+                CHANGES_EXIST="$(${pkgs.git}/bin/git status - porcelain | ${pkgs.coreutils}/bin/wc -l)"
+                if [ "$CHANGES_EXIST" -eq 0 ]; then
+                  exit 0
+                fi
+                ${pkgs.git}/bin/git pull
+                ${pkgs.git}/bin/git add .
+                ${pkgs.git}/bin/git commit -q -m "Last Sync: $(${pkgs.coreutils}/bin/date +"%Y-%m-%d %H:%M:%S")"
+                ${pkgs.git}/bin/git push -q
+              ''
+            );
+          };
+        };
 
-    systemd.user.timers.devlog-sync = {
-      Unit = { Description = "devlog periodic sync"; };
-      Timer = {
-        Unit = "devlog-sync.service";
-        OnCalendar = "*:0/30";
-      };
-      Install = { WantedBy = [ "timers.target" ]; };
-    };
+        systemd.user.timers.devlog-sync = {
+          Unit = { Description = "devlog periodic sync"; };
+          Timer = {
+            Unit = "devlog-sync.service";
+            OnCalendar = "*:0/30";
+          };
+          Install = { WantedBy = [ "timers.target" ]; };
+        };
+      }
+      (mkIf (cfg.work == false) {
+        systemd.user.services.biblelog-sync = {
+          Unit = {
+            Description = "biblelog sync";
+          };
+          Service = {
+            CPUSchedulingPolicy = "idle";
+            IOSchedulingClass = "idle";
+            ExecStart = toString (
+              pkgs.writeShellScript "biblelog-sync" ''
+                #!/usr/bin/env sh
+                DEVLOG_PATH="$HOME/repos/personal/biblelog"
+                cd $DEVLOG_PATH
+                CHANGES_EXIST="$(${pkgs.git}/bin/git status - porcelain | ${pkgs.coreutils}/bin/wc -l)"
+                if [ "$CHANGES_EXIST" -eq 0 ]; then
+                  exit 0
+                fi
+                ${pkgs.git}/bin/git pull
+                ${pkgs.git}/bin/git add .
+                ${pkgs.git}/bin/git commit -q -m "Last Sync: $(${pkgs.coreutils}/bin/date +"%Y-%m-%d %H:%M:%S")"
+                ${pkgs.git}/bin/git push -q
+              ''
+            );
+          };
+        };
 
-    systemd.user.services.biblelog-sync = {
-      Unit = { Description = "biblelog sync"; };
-      Service = {
-        CPUSchedulingPolicy = "idle";
-        IOSchedulingClass = "idle";
-        ExecStart = toString (
-          pkgs.writeShellScript "biblelog-sync" ''
-            #!/usr/bin/env sh
-            DEVLOG_PATH="$HOME/repos/personal/biblelog"
-            cd $DEVLOG_PATH
-            CHANGES_EXIST="$(${pkgs.git}/bin/git status - porcelain | ${pkgs.coreutils}/bin/wc -l)"
-            if [ "$CHANGES_EXIST" -eq 0 ]; then
-              exit 0
-            fi
-            ${pkgs.git}/bin/git pull
-            ${pkgs.git}/bin/git add .
-            ${pkgs.git}/bin/git commit -q -m "Last Sync: $(${pkgs.coreutils}/bin/date +"%Y-%m-%d %H:%M:%S")"
-            ${pkgs.git}/bin/git push -q
-          ''
-        );
-      };
-    };
-
-    systemd.user.timers.biblelog-sync = {
-      Unit = { Description = "biblelog periodic sync"; };
-      Timer = {
-        Unit = "biblelog-sync.service";
-        OnCalendar = "*:0/30";
-      };
-      Install = { WantedBy = [ "timers.target" ]; };
-    };
-  };
+        systemd.user.timers.biblelog-sync = {
+          Unit = { Description = "biblelog periodic sync"; };
+          Timer = {
+            Unit = "biblelog-sync.service";
+            OnCalendar = "*:0/30";
+          };
+          Install = { WantedBy = [ "timers.target" ]; };
+        };
+      })
+    ]);
 }
