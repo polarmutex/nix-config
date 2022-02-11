f3d924c141db0cea2592a313d14a68884a0308c9
Brian Ryall <brian@brianryall.xyz>
2022-02-11 15:31:38 -0500

    fix: dendron for work

--- users/modules/services/dendron.nix
+++ users/modules/services/dendron.nix
@@ -63,6 +63,40 @@ in
         };
       }
       (mkIf (cfg.work == false) {
+        systemd.user.services.workspace-sync = {
+          Unit = {
+            Description = "personal workspace sync";
+          };
+          Service = {
+            CPUSchedulingPolicy = "idle";
+            IOSchedulingClass = "idle";
+            ExecStart = toString (
+              pkgs.writeShellScript "workspace-sync" ''
+                #!/usr/bin/env sh
+                WORKSPACE_PATH="$HOME/repos/personal/workspace"
+                cd $WORKSPACE_PATH
+                CHANGES_EXIST="$(${pkgs.git}/bin/git status - porcelain | ${pkgs.coreutils}/bin/wc -l)"
+                if [ "$CHANGES_EXIST" -eq 0 ]; then
+                  exit 0
+                fi
+                ${pkgs.git}/bin/git pull
+                ${pkgs.git}/bin/git add .
+                ${pkgs.git}/bin/git commit -q -m "Last Sync: $(${pkgs.coreutils}/bin/date +"%Y-%m-%d %H:%M:%S")"
+                ${pkgs.git}/bin/git push -q
+              ''
+            );
+          };
+        };
+
+        systemd.user.timers.workspace-sync = {
+          Unit = { Description = "personal workspace periodic sync"; };
+          Timer = {
+            Unit = "workspace-sync.service";
+            OnCalendar = "*:0/30";
+          };
+          Install = { WantedBy = [ "timers.target" ]; };
+        };
+
         systemd.user.services.biblelog-sync = {
           Unit = {
             Description = "biblelog sync";
@@ -92,6 +126,39 @@ in
           Unit = { Description = "biblelog periodic sync"; };
           Timer = {
             Unit = "biblelog-sync.service";
+            OnCalendar = "*:0/30";
+          };
+          Install = { WantedBy = [ "timers.target" ]; };
+        };
+      })
+      (mkIf (cfg.work == true) {
+        systemd.user.services.workspace-sync = {
+          Unit = {
+            Description = "work workspace sync";
+          };
+          Service = {
+            CPUSchedulingPolicy = "idle";
+            IOSchedulingClass = "idle";
+            ExecStart = toString (
+              pkgs.writeShellScript "workspace-sync" ''
+                #!/usr/bin/env sh
+                WORKSPACE_PATH="$HOME/repos/work/workspace"
+                cd $WORKSPACE_PATH
+                CHANGES_EXIST="$(${pkgs.git}/bin/git status - porcelain | ${pkgs.coreutils}/bin/wc -l)"
+                if [ "$CHANGES_EXIST" -eq 0 ]; then
+                  exit 0
+                fi
+                ${pkgs.git}/bin/git add .
+                ${pkgs.git}/bin/git commit -q -m "Last Sync: $(${pkgs.coreutils}/bin/date +"%Y-%m-%d %H:%M:%S")"
+              ''
+            );
+          };
+        };
+
+        systemd.user.timers.workspace-sync = {
+          Unit = { Description = "work workspace periodic sync"; };
+          Timer = {
+            Unit = "workspace-sync.service";
             OnCalendar = "*:0/30";
           };
           Install = { WantedBy = [ "timers.target" ]; };
