1f2a6cd0b2e155e681498f19c7d69566d76c5104
Brian Ryall <brian@brianryall.xyz>
2022-04-27 22:29:12 -0400

    fix: sumneko lua server not building on unstable master

--- overlays/default.nix
+++ overlays/default.nix
@@ -15,4 +15,15 @@ self: super: {
   #dwm = super.callPackage ../pkgs/dwm { };
   prettierd = super.callPackage ../pkgs/prettierd { };
   stacks = super.callPackage ../pkgs/stacks { };
+  # TODO REMOVE WHEN FIXED
+  sumneko-lua-language-server = super.sumneko-lua-language-server.overrideAttrs (old: rec {
+    version = "3.2.1";
+    src = super.fetchFromGitHub {
+      owner = "sumneko";
+      repo = "lua-language-server";
+      rev = version;
+      sha256 = "sha256-rxferVxTWmclviDshHhBmbCezOI+FvcfUW3gAkBQNHQ=";
+      fetchSubmodules = true;
+    };
+  });
 }
