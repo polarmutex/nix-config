1a12baf851a0aeca5c48b7316867e20712935a8a
Brian Ryall <brian@brianryall.xyz>
2022-05-02 23:18:22 -0400

    update: beancount-language-server -> 1.1.1

--- pkgs/beancount-language-server/default.nix
+++ pkgs/beancount-language-server/default.nix
@@ -8,16 +8,16 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "beancount-language-server";
-  version = "1.0.2";
+  version = "1.1.1";
 
   src = pkgs.fetchFromGitHub {
     owner = "polarmutex";
     repo = "beancount-language-server";
     rev = "v${version}";
-    sha256 = "sha256-eojH/zjp8a8p1SKZMY5UrzxlI3XngGPIAofkWiOSIU0=";
+    sha256 = "sha256-CkwNxamkErRo3svJNth2F8NSqlJNX+1S/srKu6Z+mX4=";
   };
 
-  cargoSha256 = "sha256-LUXrTaW6aY/OPPaWFC0ejaeienrvrgasNXjenQZFEb0=";
+  cargoSha256 = "sha256-NTUs9ADTn+KoE08FikRHrdptZkrUqnjVIlcr8RtDvic=";
 
   checkInputs = lib.optional (!stdenv.isDarwin) nix;
 
