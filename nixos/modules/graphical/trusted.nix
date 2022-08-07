d21a4e842b21e8109500e29ef8fe9b06ee3bcd3b
Brian Ryall <brian@brianryall.xyz>
2022-08-07 12:12:33 -0400

    refactor: 1password

--- nixos/modules/graphical/trusted.nix
+++ nixos/modules/graphical/trusted.nix
@@ -1,14 +1,19 @@
 { pkgs, ... }: {
   programs = {
-    #_1password = {
-    #  enable = true;
-    #};
-    #_1password-gui = {
-    #  enable = true;
-    #};
+    _1password = {
+      enable = true;
+      package = pkgs.unstable._1password;
+      gid = 5001;
+    };
+    _1password-gui = {
+      enable = true;
+      package = pkgs.unstable._1password-gui;
+      polkitPolicyOwners = ["polar"];
+      gid = 5000;
+    };
   };
 
-  #security.pam.services.login.enableGnomeKeyring = true;
+  security.pam.services.login.enableGnomeKeyring = true;
 
   #services = {
   #  dbus.packages = with pkgs; [ gcr ];
