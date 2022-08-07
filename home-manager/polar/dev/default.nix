f0785819fb057c858b5957b6c20d313a40ad1e3c
Brian Ryall <brian@brianryall.xyz>
2022-08-07 12:29:09 -0400

    refactor: dev

---
+++ home-manager/polar/dev/default.nix
@@ -0,0 +1,17 @@
+    { lib, pkgs, ... }: {
+  home = {
+    packages = with pkgs; [
+      nix-update
+      nixpkgs-review
+    ];
+  };
+
+  programs = {
+    gh = {
+      enable = true;
+      settings.git_protocol = "ssh";
+    };
+
+    nix-index.enable = true;
+  };
+}
