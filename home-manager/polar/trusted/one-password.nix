f236b8b80e14b0abd6fbd720ac0c50a6fb17f9dd
Brian Ryall <brian@brianryall.xyz>
2022-08-16 21:44:09 -0400

    fix: one-pass needs started at login

--- home-manager/polar/trusted/one-password.nix
+++ home-manager/polar/trusted/one-password.nix
@@ -3,17 +3,17 @@
   home.packages = with pkgs; [
   ];
 
-  #systemd.user.services._1password = {
-  #  Unit = {
-  #    Description = "1Password";
-  #    PartOf = [ "graphical-session.target" ];
-  #  };
-  #  Service = {
-  #    Type = "simple";
-  #    ExecStart = "${pkgs._1password-gui}/bin/1password --silent";
-  #  };
-  #  Install = {
-  #    WantedBy = [ "graphical-session.target" ];
-  #  };
-  #};
+  systemd.user.services._1password = {
+    Unit = {
+      Description = "1Password";
+      PartOf = [ "graphical-session.target" ];
+    };
+    Service = {
+      Type = "simple";
+      ExecStart = "${pkgs._1password-gui}/bin/1password --silent";
+    };
+    Install = {
+      WantedBy = [ "graphical-session.target" ];
+    };
+  };
 }
