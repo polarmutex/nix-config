5c6fc33c80aec6da93e0a8a26a50113b797ccf87
Brian Ryall <brian@brianryall.xyz>
2023-02-13 14:34:54 -0500

    feat: add helix to try out

--- flake.nix
+++ flake.nix
@@ -14,139 +14,119 @@
     pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
 
     #deploy-rs.url = "github:serokell/deploy-rs";
-    #deploy-rs.inputs.nixpkgs.follows = "nixpkgs";
     lollypops.url = "github:pinpox/lollypops";
-
     monolisa-font-flake.url = "git+ssh://git@git.brianryall.xyz/polarmutex/monolisa-font-flake.git";
     #monolisa-font-flake.url = "path:///home/user/repos/personal/monolisa-font-flake";
     wallpapers.url = "git+ssh://git@git.brianryall.xyz/polarmutex/wallpapers.git";
     website.url = "git+ssh://git@git.brianryall.xyz/polarmutex/website.git";
-
     #awesome-flake = {
     #  url = "github:polarmutex/awesome-flake";
     #  inputs.nixpkgs.follows = "nixpkgs";
     #  inputs.polar-nur.follows = "polar-nur";
     #};
-    neovim-flake.url = "github:polarmutex/neovim-flake";
-    #polarmutex-blog = {
-    #  url = "github:polarmutex/brianryall.xyz";
-    #};
-
-    nur.url = "github:nix-community/NUR";
-    #nur.url = "github:nix-community/NUR";
-    #polar-nur = {
-    #  url = "github:polarmutex/nur";
-    #  inputs.neovim-flake.follows = "neovim-flake";
-    #};
-    #hardware.url = "github:nixos/nixos-hardware";
-
-    neovim.url = "github:neovim/neovim?dir=contrib";
-    #  #inputs.nixpkgs.follows = "nixpkgs";
-    #  inputs.flake-utils.follows = "flake-utils";
-    #};
     #awesome-git-src = {
     #  url = "github:awesomeWM/awesome";
     #  flake = false;
     #};
-
+    neovim-flake.url = "github:polarmutex/neovim-flake";
+    nur.url = "github:nix-community/NUR";
+    #hardware.url = "github:nixos/nixos-hardware";
+    neovim.url = "github:neovim/neovim?dir=contrib";
+    helix.url = "github:helix-editor/helix";
     #polar-dwm.url = "github:polarmutex/dwm";
     #polar-st.url = "github:polarmutex/st";
     #polar-dmenu.url = "github:polarmutex/dmenu";
     tmux-sessionizer.url = "github:polarmutex/tmux-sessionizer";
-
-    #rnix-lsp = {
-    #  url = "github:nix-community/rnix-lsp";
-    #  inputs.nixpkgs.follows = "nixpkgs";
-    #  inputs.utils.follows = "flake-utils";
-    #};
-
     #sops-nix = {
     #  url = "github:Mic92/sops-nix";
     #  inputs.nixpkgs.follows = "nixpkgs";
     #};
   };
 
-  outputs = inputs @ {
-    self,
-    flake-parts,
-    nixpkgs,
-    ...
-  }: let
-    lib = import ./lib inputs;
-  in
-    flake-parts.lib.mkFlake {inherit inputs;} {
+  outputs =
+    inputs @ { self
+    , flake-parts
+    , nixpkgs
+    , ...
+    }:
+    let
+      lib = import ./lib inputs;
+    in
+    flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         ./nixos/configurations.nix
         ./home-manager/configurations.nix
         ./pkgs
       ];
-      systems = ["x86_64-linux"];
-      perSystem = {
-        config,
-        self',
-        inputs',
-        pkgs,
-        system,
-        ...
-      }: let
-        overlays = with inputs; [
-          self.overlays.default
-          leftwm-git.overlay
-          (final: prev: {
-            neovim-polar = neovim-flake.packages.${prev.system}.default;
-          })
-          nur.overlay
-          monolisa-font-flake.overlays.default
-          (final: prev: {
-            tmux-sessionizer = tmux-sessionizer.packages.${prev.system}.default;
-          })
-          (final: prev: {
-            polar-wallpapers = wallpapers.packages.${prev.system}.polar-wallpapers;
-          })
-          (final: prev: {
-            website = website.packages.${prev.system}.site;
-          })
-          (_final: _prev: {
-            stable = import nixpkgs-stable {
-              inherit system;
+      systems = [ "x86_64-linux" ];
+      perSystem =
+        { config
+        , self'
+        , inputs'
+        , pkgs
+        , system
+        , ...
+        }:
+        let
+          overlays = with inputs; [
+            self.overlays.default
+            leftwm-git.overlay
+            (final: prev: {
+              neovim-polar = neovim-flake.packages.${prev.system}.default;
+            })
+            nur.overlay
+            monolisa-font-flake.overlays.default
+            (final: prev: {
+              tmux-sessionizer = tmux-sessionizer.packages.${prev.system}.default;
+            })
+            (final: prev: {
+              polar-wallpapers = wallpapers.packages.${prev.system}.polar-wallpapers;
+            })
+            (final: prev: {
+              website = website.packages.${prev.system}.site;
+            })
+            (_final: _prev: {
+              stable = import nixpkgs-stable {
+                inherit system;
+                config.allowUnfree = true;
+              };
+            })
+          ];
+        in
+        {
+          # Per-system attributes can be defined here. The self' and inputs'
+          # module parameters provide easy access to attributes of the same
+          # system.
+          _module.args = {
+            inherit self inputs lib;
+            pkgs = import nixpkgs {
+              inherit system overlays;
               config.allowUnfree = true;
             };
-          })
-        ];
-      in {
-        # Per-system attributes can be defined here. The self' and inputs'
-        # module parameters provide easy access to attributes of the same
-        # system.
-        _module.args = {
-          inherit self inputs lib;
-          pkgs = import nixpkgs {
-            inherit system overlays;
-            config.allowUnfree = true;
+          };
+          devShells = {
+            #default = shell {inherit self pkgs;};
+            default = pkgs.mkShell {
+              name = "nixed-shell";
+              packages = with pkgs; [
+                #inputs'.deploy-rs.packages.deploy-rs
+                #colmena
+                home-manager
+              ];
+              #shellHook = lib.optionalString (!ci) ''
+              #  ${self.checks.${pkgs.system}.pre-commit-check.shellHook}
+              #'';
+            };
+            #ci = shell {
+            #  inherit self pkgs;
+            #  ci = true;
+            #};
+          };
+          apps = {
+            #default = shell {inherit self pkgs;};
+            default = inputs'.lollypops.apps.default { configFlake = self; };
           };
         };
-        devShells = {
-          #default = shell {inherit self pkgs;};
-          default = pkgs.mkShell {
-            name = "nixed-shell";
-            packages = with pkgs; [
-              #inputs'.deploy-rs.packages.deploy-rs
-              #colmena
-              home-manager
-            ];
-            #shellHook = lib.optionalString (!ci) ''
-            #  ${self.checks.${pkgs.system}.pre-commit-check.shellHook}
-            #'';
-          };
-          #ci = shell {
-          #  inherit self pkgs;
-          #  ci = true;
-          #};
-        };
-        apps = {
-          #default = shell {inherit self pkgs;};
-          default = inputs'.lollypops.apps.default {configFlake = self;};
-        };
-      };
       flake = {
         # The usual flake attributes can be defined here, including system-
         # agnostic ones like nixosModule and system-enumerating ones, although
@@ -159,105 +139,7 @@
       };
     };
 }
-#    with inputs;
-#    let
-#    in
-#    {
-# Used with `nixos-rebuild --flake .#<hostname>`
-#      nixosConfigurations = {
-#        polarbear = nixpkgs.lib.nixosSystem {
-#          pkgs = self.legacyPackages.x86_64-linux;
-#          specialArgs = { inherit inputs; };
-#          modules = [
-#            ./nixos/polarbear/configuration.nix
-#            sops-nix.nixosModules.sops
-#          ];
-#        };
-#        polarvortex = nixpkgs.lib.nixosSystem {
-#          pkgs = self.legacyPackages.x86_64-linux;
-#          specialArgs = { inherit inputs; };
-#          modules = [
-#            ./nixos/polarvortex/configuration.nix
-#            sops-nix.nixosModules.sops
-#          ];
-#        };
-#        blackbear = nixpkgs.lib.nixosSystem {
-#          pkgs = self.legacyPackages.x86_64-linux;
-#          specialArgs = { inherit inputs; };
-#          modules = [ ./nixos/blackbear/configuration.nix ];
-#        };
-#      };
-#      homeConfigurations = {
-#        "polar@polarbear" = home-manager.lib.homeManagerConfiguration {
-#          pkgs = self.legacyPackages.x86_64-linux;
-#          extraSpecialArgs = {
-#            inherit inputs;
-#            username = "polar";
-#            features = [
-#              "desktop"
-#              "desktop/awesome.nix"
-#              "desktop/leftwm.nix"
-#              "desktop/eww.nix"
-#              "desktop/dendron.nix"
-#              "desktop/obsidian.nix"
-#              #"desktop/stacks-taskmang.nix"
-#              "dev"
-#              "trusted"
-#            ];
-#          };
-#          modules = [ ./home-manager/polar ];
-#        };
-#        "work" = home-manager.lib.homeManagerConfiguration {
-#          pkgs = self.legacyPackages."x86_64-linux";
-#          extraSpecialArgs = {
-#            inherit inputs;
-#            username = "polar";
-#            features = [
-#              "trusted"
-#              "desktop"
-#            ];
-#          };
-#          modules = [ ./home-manager/work ];
-#        };
-#        "work@redhat" = home-manager.lib.homeManagerConfiguration {
-#          pkgs = self.legacyPackages.x86_64-linux;
-#          extraSpecialArgs = {
-#            inherit inputs;
-#            username = "brian";
-#            features = [
-#            ];
-#          };
-#          modules = [ ./work_redhat ];
-#        };
-#      };
-# Hydra build jobs
-#hydraJobs."<attr>"."<system>" = derivation;
-/*
-# Hydra build jobs. Builds all configs in the CI to verify integrity
-hydraJobs = (nixpkgs.lib.mapAttrs' (name: config:
-nixpkgs.lib.nameValuePair "nixos-${name}"
-config.config.system.build.toplevel) self.nixosConfigurations);
-# // (nixpkgs.lib.mapAttrs' (name: config: nixpkgs.lib.nameValuePair
-# "home-manager-${name}" config.activation-script)
-# self.hmConfigurations);
-*/
-#      deploy = import ./deploy-rs.nix inputs;
-#      overlays = import ./overlays { inherit inputs; };
-#    }
-#    //
-#    flake-utils.lib.eachSystem [
-#      "x86_64-linux"
-#      "aarch64-linux"
-#      "aarch64-darwin"
-#      "x86_64-darwin"
-#    ]
-#      (system: {
-# Executed by `nix flake check`
-#        checks =
-#          let
-#            pkgs = self.legacyPackages."${system}";
-#          in
-#          {
+# TODO pre-commit-check
 #            pre-commit-check = pre-commit-hooks.lib.${system}.run
 #              {
 #                src = pkgs.lib.cleanSource ./.;
