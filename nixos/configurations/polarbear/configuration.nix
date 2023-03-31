694b77199a5c2359b853e8aa36f0e2f6f5534537
Brian Ryall <brian@brianryall.xyz>
2023-03-31 16:54:35 -0400

    chore: update nix flakes

--- nixos/configurations/polarbear/configuration.nix
+++ nixos/configurations/polarbear/configuration.nix
@@ -50,4 +50,5 @@
     initialHashedPassword = "$6$p/7P2dlx4xBEV72W$Ooep2JnmTJhTnexObNtAt3CNqRIhqgA2cD4bZtWMXOYAP.yBig8XToII0Fxy2Kc/Q12gep7Uqfsq6wIxRv7f21";
     shell = pkgs.fish;
   };
+  programs.fish.enable = true;
 }
