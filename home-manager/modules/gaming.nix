ac9096b6e4f23c3d0747496c27d3e8df7a4121c9
Brian Ryall <me@brianryall.xyz>
2024-03-26 14:05:28 -0400

    fix: lutris

--- home-manager/modules/gaming.nix
+++ home-manager/modules/gaming.nix
@@ -4,7 +4,7 @@
   ...
 }: {
   home.packages = with pkgs; [
-    lutris
+    (lutris.override {extraPkgs = pkgs: [pkgs.jansson];})
     inputs.nix-gaming.packages.${pkgs.system}.wine-ge
     # wineWowPackages.staging
     winetricks
