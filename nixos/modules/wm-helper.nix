d3c640524fd27bdcf017079c0e5d100929a8b991
Brian Ryall <me@brianryall.xyz>
2024-08-09 09:00:51 -0400

    fix: trace renames

--- nixos/modules/wm-helper.nix
+++ nixos/modules/wm-helper.nix
@@ -12,24 +12,27 @@
 
   # Theming helpers
   programs.dconf.enable = true;
-  services.dbus.packages = with pkgs; [dconf];
+  services = {
+    dbus.packages = with pkgs; [dconf];
 
-  services.xserver = {
-    enable = true;
-    updateDbusEnvironment = true;
-    #videoDrivers = ["modesetting"]; # Base, all hosts should set accordingly
-    layout = "us";
     libinput = {
       enable = true;
       mouse.naturalScrolling = true;
     };
 
-    desktopManager.xterm.enable = false;
-
-    windowManager.leftwm.enable = true;
-    windowManager.awesome = {
+    xserver = {
       enable = true;
-      package = inputs.awesome-flake.packages.${pkgs.system}.awesome-git;
+      updateDbusEnvironment = true;
+      #videoDrivers = ["modesetting"]; # Base, all hosts should set accordingly
+      xkb.layout = "us";
+
+      desktopManager.xterm.enable = false;
+
+      windowManager.leftwm.enable = true;
+      windowManager.awesome = {
+        enable = true;
+        package = inputs.awesome-flake.packages.${pkgs.system}.awesome-git;
+      };
     };
   };
 
