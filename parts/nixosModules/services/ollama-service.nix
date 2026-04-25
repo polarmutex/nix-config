969382f2a95588f222418ca3cf66a488b30491fc
Brian Ryall <brian@brianryall.xyz>
2026-04-25 12:43:27 -0400

    fix: ollama and webclipper

--- parts/nixosModules/services/ollama-service.nix
+++ parts/nixosModules/services/ollama-service.nix
@@ -3,16 +3,21 @@
     services.ollama = {
       enable = true;
       package = pkgs.unstable.ollama-cuda;
+      environmentVariables = {
+        OLLAMA_ORIGINS = "moz-extension://*,chrome-extension://*,safari-web-extension://*";
+        OLLAMA_CONTEXT_LENGTH = "64000";
+      };
       # Optional: preload models, see https://ollama.com/library
       loadModels = [
-        "gemma3:4b"
+        # "gemma3:4b"
         "gemma3:12b"
-        "qwen:4b"
-        "qwen3:8b"
-        "qwen3:14b"
-        "qwen2.5-coder:7b"
-        "qwen2.5-coder:14b"
-        "mxbai-embed-large"
+        "gemma4:e4b"
+        # "qwen:4b"
+        # "qwen3:8b"
+        # "qwen3:14b"
+        # "qwen2.5-coder:7b"
+        # "qwen2.5-coder:14b"
+        # "mxbai-embed-large"
       ];
     };
   };
