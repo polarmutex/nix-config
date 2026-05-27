a12bf34b5dec4424f741ea57481bb8dfcfab75e7
Brian Ryall <show>
2026-05-27 18:43:05 -0400

    fix: make morgen wrap web version

--- packages/morgen/default.nix
+++ packages/morgen/default.nix
@@ -8,12 +8,11 @@
   writeShellScript,
   makeDesktopItem,
   copyDesktopItems,
-}:
-let
+}: let
   morgenDeb = fetchurl {
     name = "morgen-4.0.5.deb";
     url = "https://dl.todesktop.com/210203cqcj00tw1/versions/4.0.5/linux/deb";
-    hash = "sha256-ZT8HC8uXDgIjDaAhivCQ9SfYYtPjYgcfCCRYkOWpLhE=";
+    hash = "sha256-McFJM23NiCqyQouavRHrfga1WK5YUs6+NxYRf5ry7h8=";
   };
 
   morgenScript = writeShellScript "morgen" ''
@@ -26,46 +25,46 @@ let
     comment = "All-in-one Calendars, Tasks and Scheduler";
     exec = "morgen %U";
     icon = "morgen";
-    categories = [ "Office" "Calendar" ];
+    categories = ["Office" "Calendar"];
     startupWMClass = "web.morgen.so";
   };
 in
-stdenv.mkDerivation {
-  pname = "morgen";
-  version = "4.0.6";
+  stdenv.mkDerivation {
+    pname = "morgen";
+    version = "4.0.6";
 
-  dontUnpack = true;
+    dontUnpack = true;
 
-  nativeBuildInputs = [
-    dpkg
-    makeWrapper
-    copyDesktopItems
-  ];
+    nativeBuildInputs = [
+      dpkg
+      makeWrapper
+      copyDesktopItems
+    ];
 
-  desktopItems = [ desktopItem ];
+    desktopItems = [desktopItem];
 
-  installPhase = ''
-    runHook preInstall
+    installPhase = ''
+      runHook preInstall
 
-    install -Dm755 ${morgenScript} $out/bin/morgen
+      install -Dm755 ${morgenScript} $out/bin/morgen
 
-    dpkg -x ${morgenDeb} $TMP/deb
-    for size in 16x16 32x32 48x48 64x64 128x128 256x256 512x512 1024x1024; do
-      src="$TMP/deb/usr/share/icons/hicolor/$size/apps/morgen.png"
-      if [ -f "$src" ]; then
-        install -Dm644 "$src" "$out/share/icons/hicolor/$size/apps/morgen.png"
-      fi
-    done
+      dpkg -x ${morgenDeb} $TMP/deb
+      for size in 16x16 32x32 48x48 64x64 128x128 256x256 512x512 1024x1024; do
+        src="$TMP/deb/usr/share/icons/hicolor/$size/apps/morgen.png"
+        if [ -f "$src" ]; then
+          install -Dm644 "$src" "$out/share/icons/hicolor/$size/apps/morgen.png"
+        fi
+      done
 
-    runHook postInstall
-  '';
+      runHook postInstall
+    '';
 
-  meta = {
-    description = "All-in-one Calendars, Tasks and Scheduler (web wrapper)";
-    homepage = "https://morgen.so/";
-    mainProgram = "morgen";
-    license = lib.licenses.unfree;
-    maintainers = with lib.maintainers; [ polarmutex ];
-    platforms = lib.platforms.linux;
-  };
-}
+    meta = {
+      description = "All-in-one Calendars, Tasks and Scheduler (web wrapper)";
+      homepage = "https://morgen.so/";
+      mainProgram = "morgen";
+      license = lib.licenses.unfree;
+      maintainers = with lib.maintainers; [polarmutex];
+      platforms = lib.platforms.linux;
+    };
+  }
